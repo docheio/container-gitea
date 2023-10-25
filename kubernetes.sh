79cf4ffbac948747672fdbceb76dba8250b2bf5a
ES-Yukun <yukun@doche.io>
2023-10-25 21:04:37 +0900

    update

--- kubernetes.sh
+++ kubernetes.sh
@@ -7,6 +7,77 @@ DB_PASSWORD=dbpasswd
 
 cat <<EOF | kubectl $MODE -n $NAMESPACE -f -
 apiVersion: v1
+kind: Secret
+metadata:
+  name: $NAME-config
+type: Opaque
+stringData:
+  app.ini: |-
+    APP_NAME = Gitea: Git with a cup of tea
+    RUN_MODE = prod
+    
+    [repository]
+    ROOT = /data/git/repositories
+    
+    [repository.local]
+    LOCAL_COPY_PATH = /data/gitea/tmp/local-repo
+    
+    [repository.upload]
+    TEMP_PATH = /data/gitea/uploads
+    
+    [server]
+    APP_DATA_PATH = /data/gitea
+    DOMAIN           = localhost
+    SSH_DOMAIN       = localhost
+    HTTP_PORT        = 3000
+    ROOT_URL         =
+    DISABLE_SSH      = false
+    SSH_PORT         = 22
+    SSH_LISTEN_PORT  = 22
+    LFS_START_SERVER = false
+    
+    [database]
+    PATH = /data/gitea/gitea.db
+    DB_TYPE = mysql
+    HOST = gitea-mariadb:3306
+    NAME = database
+    USER = $DB_USERNAME
+    PASSWD = $DB_PASSWORD
+    LOG_SQL = false
+    SSL_MODE = disable
+    
+    [indexer]
+    ISSUE_INDEXER_PATH = /data/gitea/indexers/issues.bleve
+    
+    [session]
+    PROVIDER_CONFIG = /data/gitea/sessions
+    
+    [picture]
+    AVATAR_UPLOAD_PATH = /data/gitea/avatars
+    REPOSITORY_AVATAR_UPLOAD_PATH = /data/gitea/repo-avatars
+    
+    [attachment]
+    PATH = /data/gitea/attachments
+    
+    [log]
+    MODE = console
+    LEVEL = info
+    ROOT_PATH = /data/gitea/log
+    
+    [security]
+    INSTALL_LOCK = false
+    SECRET_KEY   =
+    REVERSE_PROXY_LIMIT = 1
+    REVERSE_PROXY_TRUSTED_PROXIES = *
+    
+    [service]
+    DISABLE_REGISTRATION = false
+    REQUIRE_SIGNIN_VIEW  = false
+    
+    [lfs]
+    PATH = /data/git/lfs
+---
+apiVersion: v1
 kind: PersistentVolumeClaim
 metadata:
   name: $NAME
@@ -36,6 +107,10 @@ spec:
       containers:
       - name: $NAME
         image: gitea/gitea:latest
+        command: ["bash"]
+        args:
+        - -c
+        - mkdir -p /data/gitea/conf/ && cp /tmp/app.ini /data/gitea/conf/ && /bin/s6-svscan /etc/s6/
         imagePullPolicy: Always
         ports:
         - name: tcp3000
@@ -45,10 +120,16 @@ spec:
         volumeMounts:
         - name: $NAME
           mountPath: /data
+        - name: appinit
+          mountPath: /tmp/app.ini
+          subPath: app.ini
       volumes:
       - name: $NAME
         persistentVolumeClaim:
           claimName: $NAME
+      - name: appinit
+        secret:
+          secretName: $NAME-config
 ---
 apiVersion: v1
 kind: Service
